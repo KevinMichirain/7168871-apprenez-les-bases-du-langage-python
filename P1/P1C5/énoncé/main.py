bb399bb9a2983018cb1dc91dcc1a62606425d4d6
KevinMichirain <smichirain@gmail.com>
2026-06-17 15:06:13 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,26 @@
+nom = Michirain
+age = 29
+taille = 1.72
+est_etudiant = True
+print (nom)
+print (age)
+print (taille)
+print (est_etudiant)
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
 # Écrivez votre code ici !
+non = Michirain
+age = 29
+taille = 1.72
+est_etudiant = True
+print (nom)
+print (age)
+print (taille)
+print (est_etudiant)
+
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)

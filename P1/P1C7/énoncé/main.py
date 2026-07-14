637ecca8738aef0a204fd3811fbdde619d83d169
KevinMichirain <smichirain@gmail.com>
2026-07-14 15:56:10 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+fruits = {}
+fruits["pomme"] = "rouge"
+fruits["banane"] = "jaune"
+fruits["orange"] = "orange"
+fruits["kiwi"] = "vert"
+couleur_banane = ["fruits.values(banane)"]
+fruits["pomme"] = "vert"
+fruits.pop(banane)
+print(fruits)

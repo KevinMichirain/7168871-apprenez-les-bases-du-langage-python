f54ca4f51e6f6fd9326c574cfd213e3a6ab5ed8a
KevinMichirain <smichirain@gmail.com>
2026-07-16 17:43:56 +0100

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,15 @@
 # Écrivez votre code ici !
+#créer une variable nommé nom et y atribuer mon nom
+nom = "salim"
+
+#créer une variable nommé age et y atribuer mon àge
+age = 26
+
+#afficher une phrase contenant mes nom et àge en utilisant les variable créée
+print(f"Je m'appelle {nom} et j'ai {age} ans")
+
+#modifier la valeur de la variable age et y ajouter 10 ans a mon age actuel
+age = 36
+
+#afficher une phrase contenant mes nom et àge en utilisant les variable mises a jour
+print(f"Je m'appelle {nom} et j'ai {age} ans maintenant")
